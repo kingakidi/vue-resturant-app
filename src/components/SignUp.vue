e94800091aa87ca9175368502b5f4a197c0685fe
King Akidi <onlinemraam@gmail.com>
2022-05-17 11:13:33 -0700

    Functional Signup with json server

--- src/components/SignUp.vue
+++ src/components/SignUp.vue
@@ -2,67 +2,90 @@
   <div>
     <h1>SignUp</h1>
     <form action="" class="signup" @submit.prevent="signup()">
-     <input type="text" v-model="resturantName" placeholder="Resturant Name">
-     <input type="email"  v-model="resturantEmail" placeholder="Enter Email Address">
-     <input type="password" v-model="resturantPassword" placeholder="Enter Password">
-     <input type="submit" value="Register Resturant" >
-     <div> {{error}}</div>
+      <input type="text" v-model="resturantName" placeholder="Resturant Name" />
+      <input
+        type="email"
+        v-model="resturantEmail"
+        placeholder="Enter Email Address"
+      />
+      <input
+        type="password"
+        v-model="resturantPassword"
+        placeholder="Enter Password"
+      />
+      <input type="submit" value="Register Resturant" />
+      <div>{{ error }}</div>
     </form>
   </div>
 </template>
 <script>
-  export default {
-    name: "SignUp", 
-    data(){
-      return {
-        resturantName: "", 
-        resturantEmail: "", 
-        resturantPassword: "", 
-        error: ""
+import axios from "axios";
+export default {
+  name: "SignUp",
+  data() {
+    return {
+      resturantName: "",
+      resturantEmail: "",
+      resturantPassword: "",
+      error: "",
+    };
+  },
+  methods: {
+    clean(val) {
+      return val.length;
+    },
+    async signup() {
+      if (
+        this.clean(this.resturantName) &&
+        this.clean(this.resturantPassword) > 0 &&
+        this.clean(this.resturantPassword)
+      ) {
+        this.error = "Loading...";
+        let register = await axios.post("http://localhost:3000/users", {
+          name: this.resturantName.toLowerCase(),
+          email: this.resturantEmail.toLowerCase(),
+          password: this.resturantPassword,
+        });
+        console.log(register);
+        if (register.status === 201) {
+          this.error = "Account Created Successfully";
+          this.resturantName = "";
+          this.resturantEmail = "";
+          this.resturantPassword = "";
+          localStorage.setItem("restoUsers", JSON.stringify(register.data));
+        } else {
+          this.error = register.statusText;
+        }
+      } else {
+        this.error = "All fields required";
       }
-    }, 
-   methods: {
-     clean(val){
-       return val.length
-     },
-     signup(){
-    
-     if ( this.clean(this.resturantName) && this.clean(this.resturantPassword) > 0 && this.clean(this.resturantPassword)) {
-        // console.log(this.resturantName.length, this.resturantEmail, this.resturantPassword)
-        console.log()
-        this.error = "Loading..."
-     }else{
-       this.error = "All fields required"
-     }
-     }
-   }
-
-  }
+    },
+  },
+};
 </script>
 <style scoped>
-  div{
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-  }
-  .signup {
-    width: 350px;
-    padding: 20px;
-    background-color: #eee;
+div {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+.signup {
+  width: 350px;
+  padding: 20px;
+  background-color: #eee;
+}
+.signup input {
+  display: block;
+  margin-bottom: 10px;
+  width: 350px;
 
-  }
-  .signup input{
-    display: block;
-    margin-bottom: 10px;
-    width: 350px;
-    
-    padding: 10px;
-    box-sizing: border-box;
-  }
+  padding: 10px;
+  box-sizing: border-box;
+}
 
-  .signup input:focus {
-    outline: 1px rgba(0, 0, 0, 0.39);
-    padding: 13px;
-  } 
+.signup input:focus {
+  outline: 1px rgba(0, 0, 0, 0.39);
+  padding: 13px;
+}
 </style>
