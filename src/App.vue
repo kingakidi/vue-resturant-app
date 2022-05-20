78dc105eed04d112b2ec4d4157b0c9f40f9e4477
King Akidi <onlinemraam@gmail.com>
2022-05-20 16:45:07 +0100

    Modal Initialization

--- src/App.vue
+++ src/App.vue
@@ -55,4 +55,91 @@ export default {
   background: rgb(9, 238, 226);
   border: solid rgba(0, 0, 0, 0.39) 1px;
 }
+
+/* POP STYLING  */
+.popup-page {
+  position: fixed;
+  z-index: 20;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  background-color: rgba(0, 0, 0, 0.25);
+  display: none;
+}
+
+.popup-content {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  width: 400px;
+  transform: translate(-50%, -50%);
+  background-color: #fff;
+  background: #0e2134;
+
+  /* padding: 20px; */
+  /* height: 90%; */
+  overflow-y: auto;
+  border-radius: 5px;
+}
+.single-request {
+  /* background: #0e2134; */
+
+  color: #fff;
+  list-style: none;
+  /* margin: 20px; */
+  box-sizing: border-box;
+  border-radius: 5px;
+  padding: 20px;
+}
+
+.single-request button {
+  display: inline-block;
+  text-decoration: none;
+  color: #0e2134;
+  height: 100%;
+  padding: 15px;
+  border-radius: 3px;
+  background-color: #fff;
+  margin: 0px 10px;
+  border: none;
+  font-weight: 600;
+}
+
+/* toggle TABS  */
+
+.toggle-container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-top: 80px;
+  box-sizing: border-box;
+}
+
+.btn-toggle {
+  display: inline-block;
+  margin: 10px 0px;
+  padding: 10px 60px;
+  background-color: #ff933b;
+  color: #fff;
+  border: none;
+}
+.btn-toggle:first-child {
+  background-color: #060301;
+}
+
+.btn-toggle:hover {
+  padding: 15px 62px;
+}
+
+@media (max-width: 550px) {
+  #app {
+    width: 90%;
+  }
+}
+@media (max-width: 450px) {
+  #popup-content {
+    width: 90%;
+  }
+}
 </style>
---
+++ src/components/ModalComponent.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="popup-page" id="popup-page">
+    <div class="popup-content" id="popup-content">
+      <div class="popup-close form-group text-end">
+        <button id="popup-close" class="btn btn-danger">X</button>
+      </div>
+      <div class="show-popup-content" id="show-popup-content"></div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
